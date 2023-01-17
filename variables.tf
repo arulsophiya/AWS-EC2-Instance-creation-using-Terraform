29b3078a772f1e29d3491254abc2d56d1721680d
arulsophiya <arulsophiya.rajendran@gmail.com>
2023-01-17 14:21:08 -0800

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable region {
+  type        = string
+  description = "Specify the region for resource creation"
+}
+
+variable instance_type {
+    type = string
+    description = "Specify the instance type"
+}
+
+variable ami {
+    type = string
+    description = "Specify the ami required for the instance"
+}
